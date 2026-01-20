9377dd320f5a2122f77ed3f1687e309452b8353c
marboerq@gmail.com <marboerq@gmail.com>
2026-01-20 09:25:45 -0300

    Ajuste na conexão com Banco de dados

--- src/conectaDAO.java
+++ src/conectaDAO.java
@@ -6,15 +6,7 @@ import javax.swing.JOptionPane;
 
 
 
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 
-/**
- *
- * @author Adm
- */
 public class conectaDAO {
     
     public Connection connectDB(){
@@ -22,7 +14,7 @@ public class conectaDAO {
         
         try {
         
-            conn = DriverManager.getConnection("jdbc:mysql://localhost/uc11?user=root&password=");
+            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/uc11", "root", "Marcela2025@");
             
         } catch (SQLException erro){
             JOptionPane.showMessageDialog(null, "Erro ConectaDAO" + erro.getMessage());
