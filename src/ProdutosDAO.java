398870138b1d934cd64c4dd6fc5dcfade6a0fcb6
marboerq@gmail.com <marboerq@gmail.com>
2026-01-26 17:35:06 -0300

    Incluído método venderProduto()

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -39,7 +39,6 @@ public class ProdutosDAO {
           String sql = "SELECT * FROM produtos";
           conn = new conectaDAO().connectDB();
 
-       
              try {
             prep = conn.prepareStatement(sql);
             resultset = prep.executeQuery();
@@ -59,5 +58,26 @@ public class ProdutosDAO {
             }
             return listagem;        
     }
+    
+    public void venderProduto(int idProduto) {
+    String sql = "UPDATE produtos SET status = 'Vendido' WHERE id = ?";
+    conn = new conectaDAO().connectDB();
+
+    try {
+        prep = conn.prepareStatement(sql);
+        prep.setInt(1, idProduto);
+
+        prep.executeUpdate();
+        prep.close();
+
+        JOptionPane.showMessageDialog(null, "Produto vendido com sucesso!");
+
+    } catch (Exception e) {
+        JOptionPane.showMessageDialog(null, "Erro ao vender produto: " + e.getMessage());
+    }
+}
+    
+    
+    
 }
 
